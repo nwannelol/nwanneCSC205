a5e67daad2e42ed271267ae51a07f97d14bad7a6
nwannelol <82163363+nwannelol@users.noreply.github.com>
2022-03-05 18:44:48 +0100

    Add files via upload

---
+++ WEEK 2 OS/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float number1 = 13.5;
+    double number2 =12.4;
+
+    printf("number 1 = %f\n", number1);
+    printf("number 2 = %lf", number2);
+    return 0;
+}
